9240c9cf86e02dc05ad9534de7e251f43201f86f
s3722243 <s3722243@student.rmit.edu.au>
2018-10-17 12:24:37 +1100

    Final push

--- a3/receipt.php
+++ a3/receipt.php
@@ -47,11 +47,10 @@ topModule("Greendale Aerial Photography - Receipt");
                     <th>
                         <h3>Sub Total</h3>
                     </th>
-                </tr>
-            </tbody>
-        </table>
+                
 
         <?php
+        $totalPrice = 0;
         foreach($_SESSION['cart'] as $id => $info) {
             $oid = $info['oid'];
             $qty = $info['qty'];
@@ -59,20 +58,25 @@ topModule("Greendale Aerial Photography - Receipt");
             $price = (int)$services[substr($id,0,-1)][$oid]['Price'] * $qty;
             $totalPrice += $price;
             $title = substr($id,0,-1). " minutes";
+            $oldOid = $oid;
             $oid = ($oid=='A' ? '1080p' : '4K');
         
             $records[] = date("d/m/Y");
             array_push($records, $_SESSION['customerDetails']['firstName'] . " " . $_SESSION['customerDetails']['lastName'],$_SESSION['customerDetails']['email'],$_SESSION['customerDetails']['phone'],$_SESSION['customerDetails']['address'],
-            substr($id,0,-1),$oid,$qty,$services[substr($id,0,-1)][$oid]['Price'],$price);
+            substr($id,0,-1),$oid,$qty,$services[substr($id,0,-1)][$oldOid]['Price'],$price);
             echo "<tr>";
             foreach($records as $record) {
                 echo "<td><p>$record</p></td>";
             }
             echo "</tr>";
             writeCSV("orders.txt",$records);
-            }
+        }
 
         ?>
+
+        </tr>
+            </tbody>
+        </table>
     </section>
 </article>
 
